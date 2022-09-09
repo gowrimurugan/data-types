3ea7c4ff6deaf8cb8c6a5e38b350bc317fde16b9
gowrimurugan <107912689+gowrimurugan@users.noreply.github.com>
2022-09-09 01:59:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//premitive data type
+let a;
+console.log(typeof a);
+
+let b=3;
+console.log(b, typeof b);
+
+let c = 10;
+let d = "10";
+let e = (c==d);
+let j = (c===d);
+console.log(e, typeof e);
+console.log(j, typeof j);
+
+var phone =  "iphone";
+var phone = "hlo"
+console.log(phone , typeof phone);
+
+var f = null;
+var f = 1
+console.log(f, typeof f);
+
+var g = 4;
+var h = "a";
+var i = g / h;
+console.log(i, typeof i);
+
+//reference data types
+
+var fruits = {
+    fruit1 : "apple",
+    fruit2 : "banana",
+    count : 8,
+}
+console.log(typeof fruits);
+
+let colors = ["lavender","red", "blue","pink"]
+console.log(typeof colors);
